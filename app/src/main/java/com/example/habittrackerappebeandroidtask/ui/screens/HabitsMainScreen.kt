24c468e0cd26d5ceda7759dc0d1c200e36127df1
abdelrahman <abdelrahman.mohamed2572@gmail.com>
2025-02-16 23:24:12 +0200

    final version

--- app/src/main/java/com/example/habittrackerappebeandroidtask/ui/screens/HabitsMainScreen.kt
+++ app/src/main/java/com/example/habittrackerappebeandroidtask/ui/screens/HabitsMainScreen.kt
@@ -7,7 +7,9 @@ import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
@@ -17,12 +19,18 @@ import androidx.compose.material.icons.filled.Add
 import androidx.compose.material.icons.filled.History
 import androidx.compose.material3.FloatingActionButton
 import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
@@ -35,12 +43,23 @@ import com.example.habittrackerappebeandroidtask.ui.viewmodel.SharedViewModel
 import com.example.habittrackerappebeandroidtask.ui.viewmodel.SharedViewModelContract
 import com.google.gson.Gson
 import kotlinx.coroutines.flow.collect
+import java.time.LocalDate
+import java.time.format.DateTimeFormatter
 
 @RequiresApi(Build.VERSION_CODES.O)
 @Composable
 fun HabitsMainScreen(modifier: Modifier = Modifier, setSelectedHabit:(HabitUI)->Unit, state: SharedViewModelContract.State, toggleHabitStatus:(Int, Boolean)->Unit, navigate:(Route)->Unit ) {
+    var currentDate by remember { mutableStateOf(LocalDate.now()) }
+
     Box(modifier.fillMaxSize()) {
         Column(Modifier.fillMaxSize()) {
+            Text(
+                text = currentDate.format(DateTimeFormatter.ofPattern("dd MMMM yyyy")),
+                style = MaterialTheme.typography.titleLarge,
+                fontWeight = FontWeight.Bold,
+                modifier = Modifier.padding(bottom = 8.dp)
+            )
+            Spacer(Modifier.height(10.dp))
             MainHeaderComposable(progress = state.progress , completedHabits = state.completedHabits , totalHabits = state.totalHabits)
 
             LazyColumn(
